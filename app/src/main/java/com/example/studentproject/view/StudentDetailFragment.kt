43f6842b9a281da7ade7c67796f35de2024ebb85
michaelsheyrt <michaelsheyrt@gmail.com>
2026-04-08 08:50:59 +0700

    Update code fetch id student di DetailViewModel.kt dan merubah student menjadi studen.id di StudentDetailFragment.kt

--- app/src/main/java/com/example/studentproject/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/studentproject/view/StudentDetailFragment.kt
@@ -31,7 +31,7 @@ class StudentDetailFragment : Fragment() {
 
         //instantiate viewModel
         viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
-        viewModel.fetch(student)
+        viewModel.fetch(student.id)
 
         observeViewModel()
     }
