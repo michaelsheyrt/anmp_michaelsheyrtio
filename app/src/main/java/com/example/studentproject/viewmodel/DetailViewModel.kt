104a9b008b0312ea0f81990eb284fd184770ef60
michaelsheyrt <michaelsheyrt@gmail.com>
2026-04-08 08:37:26 +0700

    Update code Log error dengan error value di DetailViewModel.kt

--- app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.studentproject.viewmodel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -12,14 +13,21 @@ import com.example.studentproject.model.Student
 
 class DetailViewModel(application: Application): AndroidViewModel(application) {
     val studentLD = MutableLiveData<Student>()
+    val errorLD = MutableLiveData<Boolean>()
     val TAG:String = "volley Tag"
     var queue: RequestQueue?=null
 
     fun fetch(student: Student) {
         queue = Volley.newRequestQueue(getApplication())
         val url = "https://www.jsonkeeper.com/b/LLMW"
+        errorLD.value = false
 
-        val stringRequest = StringRequest(Request.Method.GET, url, {},{})
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            {},
+            {
+                Log.d("volley_status", it.message.toString())
+                errorLD.value = true
+            })
         stringRequest.tag = TAG
         queue?.add(stringRequest)
 
